f139f886d38c6cae75c3ac6e33a4bf12fc2ccd2d
Laura Fiig <Laura Fiig>
2016-12-01 23:56:05 -0600

    read file working, homework complete

--- liri.js
+++ liri.js
@@ -113,6 +113,71 @@ request(queryUrl, function(error, response, body) {
 }
 //************************************
 
+
+//**********read the file function**********
+function readFile() {
+  // Load the fs package to read and write
+var fs = require("fs");
+  // read the insruction file
+  fs.readFile("random.txt", "utf8", function(err, data) {
+    // Break down the command inside
+    data = data.split(",");
+    // into directions
+    var doWhat = data[0];
+    var doTo = data[1];
+//and do what it says
+//if tweets are requested
+if (doWhat === "my-tweets") {
+  //call tweet function
+  myTweets()
+//if spotify is requested
+} else if (doWhat === "spotify-this-song") {
+  //call spotify function
+  var songName = doTo
+  var spotify = require('spotify');
+  // request to spotify 
+spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    // log data
+     console.log("-------------------------------")
+     console.log("Performed by the Artist: "+ data.tracks.items[0].artists[0].name)
+     console.log("-------------------------------")
+     console.log("Your song's name: "+ data.tracks.items[0].name)
+     console.log("-------------------------------")
+     console.log("From the album: "+ data.tracks.items[0].album.name)
+     console.log("-------------------------------")
+     console.log("Preview your song at: "+ data.tracks.items[0].preview_url)
+     console.log("-------------------------------")     
+});
+//if movie data is requested
+} else if (doWhat === "movie-this") {
+  var movieName = doTo
+var request = require("request");
+// request to OMDB 
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&tomatoes=true&r=json";
+request(queryUrl, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    // log the data
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Country Produced: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+    console.log("Rotten Tomatoes Rating.: " + JSON.parse(body).tomatoRating);
+    console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);        
+  }
+});
+} 
+});
+}
+//************************************
+
+//**********read inputs and**********
 //**********run section**********
 //if tweets are requested
 if (process.argv[2] === "my-tweets") {
@@ -125,6 +190,8 @@ if (process.argv[2] === "my-tweets") {
 //if movie data is requested
 } else if (process.argv[2] === "movie-this") {
   movieThis()
+//do-what-it-says
+} else if (process.argv[2] === "do-what-it-says") {
+  readFile()
 }
 
-//do-what-it-says
